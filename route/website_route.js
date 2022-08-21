7ab65db3bffe3b5943e200e5f7dad2e7526d8bac
RajKKapadia <raajforyou@gmail>
2022-08-21 16:54:28 +0530

    added cors for website route

--- route/website_route.js
+++ route/website_route.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const cors = require('cors')
 const router = express.Router();
 
 const DIALOGFLOW_API = require('../helper/dialogflow_api');
 const UTIL = require('../controller/util');
 
-router.post('/website', async (req, res) => {
+router.post('/website', cors(), async (req, res) => {
 
     console.log('A new request came at website.');
     console.log(req.body);
